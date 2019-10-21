3bfcef176315058a59ec7a8bbc852c3228c7011f
Ryan M. Cosheril <Ryan.M.Cosheril@students.ittralee.ie>
2019-10-21 15:57:53 +0100

    got a bar of somethin yo

--- ComputerGraphicsCA2/ComputerGraphicsCA2/Assets/TestOutcode.cs
+++ ComputerGraphicsCA2/ComputerGraphicsCA2/Assets/TestOutcode.cs
@@ -1,13 +1,126 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TestOutcode : MonoBehaviour
 {
+
+    Texture2D texture;
     void Start()
     {
-        Texture2D texture = new Texture2D(500, 500);
+        texture = new Texture2D(500, 500);
         GetComponent<Renderer>().material.mainTexture = texture;
+
+        Vector3[] cube = new Vector3[8];
+        cube[0] = new Vector3(1, 1, 1);
+        cube[1] = new Vector3(-1, 1, 1);
+        cube[2] = new Vector3(-1, -1, 1);
+        cube[3] = new Vector3(1, -1, 1);
+        cube[4] = new Vector3(1, 1, -1);
+        cube[5] = new Vector3(-1, 1, -1);
+        cube[6] = new Vector3(-1, -1, -1);
+        cube[7] = new Vector3(1, -1, -1);
+
+        Vector3 cameraPosition = new Vector3(0, 0, 10);
+        Vector3 cameraLookAt = new Vector3(0, 0,0 );
+        Vector3 cameraUp = new Vector3(0,1,0);
+
+        Vector3 lookRotationDir = cameraLookAt - cameraPosition;
+
+        Quaternion camRotation = Quaternion.LookRotation(lookRotationDir.normalized, cameraUp.normalized);
+
+        Matrix4x4 viewingMatrix = Matrix4x4.TRS(-cameraPosition, camRotation, Vector3.one);
+
+        Matrix4x4 projectionMatrix = Matrix4x4.Perspective(45, 1.6f, 1, 1000);
+
+        Matrix4x4 BIGMATRIX = projectionMatrix * viewingMatrix;
+
+        Vector3[] finalImage = MatrixTransform(cube, BIGMATRIX);
+
+        Vector2[] finalPostDivisionIMage = dividebyz(finalImage);
+
+        // 0- 1
+
+        Vector2 start = finalPostDivisionIMage[0];
+        Vector2 finish = finalPostDivisionIMage[1];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        // 1-2
+
+        start = finalPostDivisionIMage[1];
+        finish = finalPostDivisionIMage[2];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        // 2-3
+
+        start = finalPostDivisionIMage[2];
+        finish = finalPostDivisionIMage[3];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        // 3-0
+
+        start = finalPostDivisionIMage[3];
+        finish = finalPostDivisionIMage[0];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        start = finalPostDivisionIMage[1];
+        finish = finalPostDivisionIMage[5];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        start = finalPostDivisionIMage[0];
+        finish = finalPostDivisionIMage[4];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        start = finalPostDivisionIMage[2];
+        finish = finalPostDivisionIMage[6];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        start = finalPostDivisionIMage[3];
+        finish = finalPostDivisionIMage[7];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        start = finalPostDivisionIMage[5];
+        finish = finalPostDivisionIMage[4];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        start = finalPostDivisionIMage[4];
+        finish = finalPostDivisionIMage[7];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        start = finalPostDivisionIMage[7];
+        finish = finalPostDivisionIMage[6];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        start = finalPostDivisionIMage[6];
+        finish = finalPostDivisionIMage[5];
+
+        if (lineClip(ref start, ref finish))
+            plot(breshenham(convertToScreenSpace(start), convertToScreenSpace(finish)));
+
+        // 1 - 5
 
         Vector2 point1 = new Vector2(.5f, 2f);
         Vector2 point2 = new Vector2(.3f, .1f);
@@ -47,13 +160,39 @@ public class TestOutcode : MonoBehaviour
         //foreach(Vector2Int v in list)
         //    Debug.Log(v.x + "  ,  " + v.y);
         
-        foreach(Vector2Int v in list)
+        
+
+      
+    }
+
+    private void plot(List<Vector2Int> list)
+    {
+        foreach (Vector2Int v in list)
         {
             Color color = Color.blue;
             texture.SetPixel(v.x, v.y, color);
         }
 
         texture.Apply();
+    }
+
+    private Vector2Int convertToScreenSpace(Vector2 v)
+    {
+        int x = (int)Math.Round(((v.x + 1) / 2) * (Screen.width - 1));
+
+        int y = (int)Math.Round(((1 - v.y) / 2) * (Screen.height - 1));
+
+        return new Vector2Int(x, y);
+    }
+
+    private Vector2[] dividebyz(Vector3[] finalImage)
+    {
+        List<Vector2> output_list = new List<Vector2>();
+        foreach (Vector3 v in finalImage)
+            output_list.Add(new Vector2(v.x / v.z, v.y / v.z));
+
+        return output_list.ToArray();
+
     }
 
     // Update is called once per frame
@@ -99,25 +238,25 @@ public class TestOutcode : MonoBehaviour
         {
             v = intercept(u, v, 0);
 
-            return false;
+            return true;
         }
 
         if (v0.down)
         {
             v = intercept(u, v, 1);
 
-            return false;
+            return true;
         }
 
         if (v0.left)
         {
             v = intercept(u, v, 2);
 
-            return false;
+            return true;
         }
 
         v = intercept(u, v, 3);
-        return false;
+        return true;
     }
 
     /// <summary>
@@ -222,5 +361,21 @@ public class TestOutcode : MonoBehaviour
     public Vector2Int swapXY(Vector2Int point)
     {
         return new Vector2Int(point.y, point.x);
+    }
+
+    private Vector3[] MatrixTransform(
+       Vector3[] meshVertices,
+       Matrix4x4 transformMatrix)
+    {
+        Vector3[] output = new Vector3[meshVertices.Length];
+        for (int i = 0; i < meshVertices.Length; i++)
+            output[i] = transformMatrix *
+                new Vector4(
+                meshVertices[i].x,
+                meshVertices[i].y,
+                meshVertices[i].z,
+                    1);
+
+        return output;
     }
 }
